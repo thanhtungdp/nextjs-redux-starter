11c2d5b260c6511f2f743136df05b33201dd9b61
thanhtungdp <tungptkh@gmail.com>
2017-05-11 12:09:03 +0700

    custom .babelrc

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,42 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import {autobind} from 'core-decorators';
 import Layout from "../components/Layout";
 import fetch from 'isomorphic-unfetch'
 
-const Index = (props) => (
-	<Layout>
-		<h1>Batman Movies</h1>
-		<ul>
-			{props.movies.map((movie) => (
-				<li key={movie.imdbID}>
-					<Link as={`/p/${movie.imdbID}`} href={`/post?id=${movie.imdbID}`}>
-						<a>{movie.Title}</a>
-					</Link>
-				</li>
-			))}
-		</ul>
-	</Layout>
-)
+class Movies extends React.Component{
 
-Index.getInitialProps = async function() {
-	const res = await fetch('http://www.omdbapi.com/?s=batman')
-	const data = await res.json()
-
-	console.log(`Movie data fetched. Count: ${data.Search.length}`)
-
-	return {
-		movies: data.Search
-	}
 }
 
-export default Index
+@autobind
+export default class Index extends React.Component{
+	static getInitialProps = async function() {
+		const res = await fetch('http://www.omdbapi.com/?s=batman')
+		const data = await res.json()
+
+		console.log(`Movie data fetched. Count: ${data.Search.length}`)
+
+		return {
+			movies: data.Search
+		}
+	}
+
+	render(){
+		return <Layout>
+			<Head>
+				<title>Danh sach ne hay qua</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<h1>Batman Movies</h1>
+			<ul>
+				{this.props.movies.map((movie) => (
+					<li key={movie.imdbID}>
+						<Link as={`/p/${movie.imdbID}`} href={`/post?id=${movie.imdbID}`}>
+							<a>{movie.Title}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</Layout>
+	}
+}
--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,13 @@
 import Layout from "../components/Layout";
+import Head from 'next/head'
 import fetch from "isomorphic-unfetch";
 
 const Post = props => (
   <Layout>
+    <Head>
+      <title>{props.movie.Title}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1>{props.movie.Title}</h1>
     <p>{props.movie.Plot}</p>
     <img src={props.movie.Poster} />
